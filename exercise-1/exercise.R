0ff9f3cb5785b1d69c29bb7229714d98fa7cecb3
Michael Freeman <michael.k.freeman@gmail.com>
2018-04-03 07:54:16 -0700

    Remove extra back-tick

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -9,7 +9,7 @@ my_lunch <- c("soup", "pb + j")
 # Create a list `meals` that has contains your breakfast and lunch
 meals <- list(breakfast = my_breakfast, lunch = my_lunch)
 
-# Add a "dinner" element to your `meals`` list that has what you plan to eat 
+# Add a "dinner" element to your `meals` list that has what you plan to eat 
 # for dinner
 meals$dinner <- c("curry", "rice")
 
@@ -19,7 +19,7 @@ dinner <- meals$dinner
 
 # Use double-bracket notation to extract your `lunch` element from your list
 # and save it in your list as the element at index 5 (no reason beyond practice)
-meals[[5]] <- meals[['lunch']]
+meals[[5]] <- meals[["lunch"]]
 
 # Use single-bracket notation to extract your breakfast and lunch from your list
 # and save them to a list called `early_meals`
